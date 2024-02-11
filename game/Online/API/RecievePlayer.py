7ef567ace3385958a5dbfead43904380270ece43
GDcheerios <minecreeper0913@gmail.com>
2024-02-11 07:43:53 -0600

    made online fight possible

---
+++ game/Online/API/RecievePlayer.py
@@ -0,0 +1,21 @@
+# game packages
+# graphics packages
+from Graphics.Status import Status
+
+# external packages
+import requests
+
+# built-in packages
+import json
+
+
+def receive_player(username_or_id, server_url):
+    find_player_status = Status("Finding player")
+    find_player_status.start()
+    result = requests.get(f"{server_url}/api/account/receive/{username_or_id}").text
+    find_player_status.stop()
+
+    if result != "Not Found":
+        result = json.loads(result)
+
+    return result
